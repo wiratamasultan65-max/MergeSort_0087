f66debb1baae7d7a3cb778748f66258015c3863a
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-02 19:09:57 +0800

    step 5

--- Merge_Short.cpp
+++ Merge_Short.cpp
@@ -79,3 +79,10 @@ void void mergeSort(int low, int high)
         i++;           // step 4.f.ii
         k++;           // step 4.f.iii
     }
+
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
