90491b754e67b6eeebfe4dc0c8e181ac49850031
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-02 19:04:08 +0800

    commit

--- Merge_Short.cpp
+++ Merge_Short.cpp
@@ -2,3 +2,33 @@
 using namespace std;
 //Membbuat main array dan temporary array
 int arr[20], B[20];
+
+// n is array input size
+int n;
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+    cout << "\n-----------------------" << endl;
+    cout << "\nInputkan Isi elemen array" << endl;
+    cout << "\n-----------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke-" << i << " : ";
+        cin >> arr[i];
+    }
+}
