c962070244a15df2424dbb44e207dab0a37e8704
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-04-02 19:07:55 +0800

    step 3

--- Merge_Short.cpp
+++ Merge_Short.cpp
@@ -40,3 +40,9 @@ void void mergeSort(int low, int high)
         return; // step 1.a
     }
     int mid = (low + high) / 2; // step 2
+
+    // step 3
+    // fungsi rekursif - memanggil diri sendiri
+    mergeSort(low, mid); // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
+    
